3e5b41f0846d590ec90d25013b914be00ab407ad
Joseph DePumpo <joe@depumpo.com>
2026-04-29 19:38:01 -0400

    configure opencloud

--- services/opencloud.nix
+++ services/opencloud.nix
@@ -1,4 +1,11 @@
-{...}: {
+{config, ...}: {
+  sops.templates."opencloud-env" = {
+    content = ''
+      NOTIFICATIONS_SMTP_PASSWORD=${config.sops.placeholder."authelia-smtp-password"}
+    '';
+    owner = "opencloud";
+  };
+
   environment.persistence."/nix/persist".directories = [
     {
       directory = "/var/lib/opencloud";
@@ -18,9 +25,16 @@
   services.opencloud = {
     enable = true;
     url = "https://cloud.clift.one";
+    environmentFile = config.sops.templates."opencloud-env".path;
     environment = {
       OC_INSECURE = "true";
       OC_LOG_LEVEL = "error";
+      OC_SHARING_PUBLIC_SHARE_MUST_HAVE_PASSWORD = "false";
+      NOTIFICATIONS_SMTP_HOST = "mail.depumpo.com";
+      NOTIFICATIONS_SMTP_PORT = "465";
+      NOTIFICATIONS_SMTP_SENDER = "CliftONE Cloud <cloud@jfd.is>";
+      NOTIFICATIONS_SMTP_USERNAME = "links@jfd.is";
+      NOTIFICATIONS_SMTP_ENCRYPTION = "ssl";
     };
   };
 }
