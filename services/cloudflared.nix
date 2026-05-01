26036224f1bf4a623784241a816ae6e19b41064b
Joseph DePumpo <joe@depumpo.com>
2026-05-01 14:11:22 -0400

    add traefik http to https redirect

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -8,7 +8,7 @@
     enable = true;
     tunnels."3ef0f677-42ba-441c-8192-72f4d66b6dad" = {
       credentialsFile = config.sops.secrets."cloudflare-tunnel".path;
-      ingress."*.clift.one" = "http://localhost:80";
+      ingress."*.clift.one" = "http://localhost:8880";
       default = "http_status:404";
     };
   };
